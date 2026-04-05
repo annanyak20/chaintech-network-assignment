8ba91033dd182774a387b03c1158706701e81926
annanya <annanya.keshav@gmail.com>
2026-04-05 18:05:31 +0530

    Final fixes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const router =  require('./routes/taskRoutes.js');
 
 app.use(express.json());
 app.use('/tasks',router );
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => {
     console.log("Server is listening to port " + PORT)
 })
